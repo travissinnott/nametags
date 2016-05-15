e16f116dca80c84d181bf8736c92c0bb5564fa21
Travis Sinnott <travis.sinnott@gmail.com>
2016-05-14 21:27:22 -0400

    Centered layout, added guide lines and red lines

--- nametags.js
+++ nametags.js
@@ -1,42 +1,54 @@
 var doc = new jsPDF("portrait", "in", "letter");
 // 3.5 x 2.25 inches
 
-/*
-doc.setLineWidth(14);
-doc.setDrawColor(96,0,0);
-doc.line(72, 72, 144, 72); // horizontal line
-*/
-
-var MARGIN = {
-    top: 0.25,
-    left: 0.25,
-    right: 0.25,
-    bottom: 0.25
+var Page = function(width, height, margin){
+    this.w = width;
+    this.h = height;
+    this.m = margin;
+    this.center = {
+        x: width / 2,
+        y: height / 2
+    };
+    return this;
 };
-var CENTER = { x:4.25, y:5.5 };
+
 var dim = { h: 2.25, w: 3.5 };
+var page = new Page(8.5, 11, 0.25);
+
 
 doc.setDrawColor(0,0,255);
 doc.setLineWidth(0.01);
-doc.line(CENTER.x, 0.25, CENTER.x, 10.75);
-doc.line(0.25, CENTER.y, 8.25, CENTER.y);
+doc.line(page.center.x, 0, page.center.x, page.h);
+doc.line(0, page.center.y, page.w, page.center.y);
+
+var rows = Math.floor(page.h / dim.h);
+var cols = Math.floor(page.w / dim.w);
+console.log("Rows: "+rows);
+console.log("Cols: "+cols);
+
+
+doc.setDrawColor(192,192,192);
+doc.setLineWidth(0.01);
 
 //rows
-for(var i=0; i<4; i++) {
-    var y = CENTER.y-(2*dim.h) + (dim.h*i);
-    
-    //columns
-    for (var col=0; col<2; col++) {
-        var x = (col==0) ? CENTER.x - dim.w : CENTER.x;
-        
-        //draw the box
-        doc.setLineWidth(0.014);
-        doc.setDrawColor(0,0,0);
-        doc.rect(x, y, dim.w, dim.h);
-    }
-    //doc.setLineWidth(0.1);
-    doc.setDrawColor(112,0,0);
-    //doc.line(MARGIN.left, y + 0.1, 8, y + 0.1); // horizontal line
-    //doc.line(MARGIN.left, y + h - 0.3, 8, y + h - 0.3); // horizontal line
-    //doc.rect(x, y+0.1, 8, 0.2);
+for (var row=0; row <= rows; row++) {
+    var y = page.center.y - dim.h * rows/2 + (row * dim.h);
+    console.log("Row at: "+y);
+    doc.line(0.5, y, 8, y);
+}
+
+//cols
+for (var col=0; col < rows+1; col++) {
+    var x = page.center.x - dim.w * cols/2 + col * dim.w;
+    console.log("Col at: "+x);
+    doc.line(x, 0.5, x, 10.5);
+}
+
+for(var row=0; row<rows; row++) {
+    var y = page.center.y - dim.h * rows/2 + (row * dim.h);
+
+    doc.setDrawColor(192,0,0);
+    doc.setLineWidth(0.2);
+    doc.line(page.m, y+0.2, page.w-page.m, y+0.2);
+    doc.line(page.m, y+dim.h-0.2, page.w-page.m, y+dim.h-0.2);
 }
