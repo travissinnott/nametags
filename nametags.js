093f103a004f9be0a3d5cd565c802963b484f22e
Travis Sinnott <travis.sinnott@gmail.com>
2016-05-14 21:44:55 -0400

    Near perfect replica

--- nametags.js
+++ nametags.js
@@ -15,18 +15,19 @@ var Page = function(width, height, margin){
 var dim = { h: 2.25, w: 3.5 };
 var page = new Page(8.5, 11, 0.25);
 
-
+//Draw Center Axis
 doc.setDrawColor(0,0,255);
 doc.setLineWidth(0.01);
 doc.line(page.center.x, 0, page.center.x, page.h);
 doc.line(0, page.center.y, page.w, page.center.y);
 
+//Calculate number of rows and cols
 var rows = Math.floor(page.h / dim.h);
 var cols = Math.floor(page.w / dim.w);
 console.log("Rows: "+rows);
 console.log("Cols: "+cols);
 
-
+//Draw cut lines
 doc.setDrawColor(192,192,192);
 doc.setLineWidth(0.01);
 
@@ -44,6 +45,11 @@ for (var col=0; col < rows+1; col++) {
     doc.line(x, 0.5, x, 10.5);
 }
 
+//doc.setFont("helvetica");
+doc.setFont("times");
+//doc.setFontType("bold");
+
+//Draw Badges
 for(var row=0; row<rows; row++) {
     var y = page.center.y - dim.h * rows/2 + (row * dim.h);
 
@@ -51,4 +57,16 @@ for(var row=0; row<rows; row++) {
     doc.setLineWidth(0.2);
     doc.line(page.m, y+0.2, page.w-page.m, y+0.2);
     doc.line(page.m, y+dim.h-0.2, page.w-page.m, y+dim.h-0.2);
+
+    for (var col=0; col<cols; col++){
+        var x = page.center.x - dim.w * cols/2 + col * dim.w;
+        doc.setFont("times");
+        doc.setFontSize(64);
+        doc.text(x+0.3, y+dim.h-1, 'Travis');
+        doc.setFontSize(18);
+        doc.text(x+0.4, y+dim.h-0.65, 'Sinnott');
+        doc.setFont("helvetica");
+        doc.setFontSize(8);
+        doc.text(x+0.2, y+dim.h-0.4, 'TEAM, MANAGEMENT, AND LEADERSHIP PROGRAM');        
+    }
 }
